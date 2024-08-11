60aa972c24404c1bdb0f355f3861f4586a7225bd
wang <285665110@qq.com>
2024-08-11 18:56:40 +0800

    优化

--- oop/src/com/wangxu/innerclass/Outer.java
+++ oop/src/com/wangxu/innerclass/Outer.java
@@ -1,8 +1,10 @@
 package com.wangxu.innerclass;
+
 // 外部类
 public class Outer {
     // 成员内部类: 无static修饰,属于外部类的对象持有的
-    public  class Inner {
+    public class Inner {
+        
         public void show() {
             System.out.println("show");
         }
